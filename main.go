af3ec7e5b78f39a07ec3a1d1cdb9dce2f5f1880d
Fernando de Paula <fernando.maciel@q2pay.com.br>
2021-06-28 11:50:45 -0300

    feat: crud ok

--- main.go
+++ main.go
@@ -20,15 +20,15 @@ func main() {
 
 	//Receitas
 	router.HandleFunc("/recipes", server.CreateRecipe).Methods(http.MethodPost)
-	router.HandleFunc("/recipes", server.ShowRecipes).Methods(http.MethodGet)
+	router.HandleFunc("/recipes/category/{id}", server.ShowRecipes).Methods(http.MethodGet)
 	router.HandleFunc("/recipes/{id}", server.UpdateRecipe).Methods(http.MethodPut)
 	router.HandleFunc("/recipes/{id}", server.DeleteRecipe).Methods(http.MethodDelete)
 
 	//Ingredientes
-	// router.HandleFunc("/categories", server.CreateCategory).Methods(http.MethodPost)
-	// router.HandleFunc("/categories", server.ShowCategories).Methods(http.MethodGet)
-	// router.HandleFunc("/categories/{id}", server.UpdateCategory).Methods(http.MethodPut)
-	// router.HandleFunc("/categories/{id}", server.DeleteCategory).Methods(http.MethodDelete)
+	router.HandleFunc("/ingredients", server.CreateIngredient).Methods(http.MethodPost)
+	router.HandleFunc("/ingredients/recipe/{id}", server.ShowIngredients).Methods(http.MethodGet)
+	router.HandleFunc("/ingredients/{id}", server.UpdateIngredient).Methods(http.MethodPut)
+	router.HandleFunc("/ingredients/{id}", server.DeleteIngredient).Methods(http.MethodDelete)
 
 	fmt.Println("Server listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- server/server.go
+++ server/server.go
@@ -250,12 +250,11 @@ func CreateRecipe(rw http.ResponseWriter, r *http.Request) {
 
 	//status codes
 	rw.WriteHeader(201)
-	rw.Write([]byte(fmt.Sprintf("Type inserted with id: %d", insertedId)))
+	rw.Write([]byte(fmt.Sprintf("Recipe inserted with id: %d", insertedId)))
 
 }
 
 func ShowRecipes(rw http.ResponseWriter, r *http.Request) {
-
 
 	//Pegando os parametros da requisição
 	parameters := mux.Vars(r) //pega os params, é retornado um MAP de string
@@ -275,7 +274,7 @@ func ShowRecipes(rw http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	//Buscar todos os registros no banco
-	lines, error := db.Query("select * from Recipes where id=?", ID)
+	lines, error := db.Query("select * from Recipes where categoryId=?", ID)
 	if error != nil {
 		rw.Write([]byte("Error when search recipes"))
 		return
@@ -285,27 +284,286 @@ func ShowRecipes(rw http.ResponseWriter, r *http.Request) {
 
 	var recipes []recipe
 	for lines.Next() {
-		var category category
+		var recipe recipe
 
-		if error := lines.Scan(&category.ID, &category.Name, &category.Image); error != nil {
-			rw.Write([]byte("Error when scanning categorys"))
+		if error := lines.Scan(&recipe.ID, &recipe.Name, &recipe.Description, &recipe.Image, &recipe.CategoryId, &recipe.Prepare_mode); error != nil {
+			rw.Write([]byte("Error when scanning recipes"))
 			return
 		}
 
-		categories = append(categories, category)
+		recipes = append(recipes, recipe)
 	}
 
 	rw.WriteHeader(http.StatusOK)
-	if error := json.NewEncoder(rw).Encode(categories); error != nil {
+	if error := json.NewEncoder(rw).Encode(recipes); error != nil {
 		rw.Write([]byte("Erro when convert categorys to JSON"))
 		return
 	}
 }
 
 func UpdateRecipe(rw http.ResponseWriter, r *http.Request) {
+	parameters := mux.Vars(r)
+	ID, error := strconv.ParseInt(parameters["id"], 10, 32)
+	if error != nil {
+		rw.Write([]byte("Error when convert parameter to integer"))
+		return
+	}
+
+	//Pegando o corpo da requisição
+	requestBody, error := ioutil.ReadAll(r.Body)
+	if error != nil {
+		rw.Write([]byte("Error when read request body"))
+		return
+	}
+
+	//Convertendo o corpo da requisição para um struct
+	var recipe recipe
+	if error := json.Unmarshal(requestBody, &recipe); error != nil {
+		rw.Write([]byte("Error when convert recipe to struct"))
+		return
+	}
+
+	//conectando com o banco
+	db, error := database.Connect()
+	if error != nil {
+		rw.Write([]byte("Error to connect to database"))
+		return
+	}
+
+	defer db.Close()
+
+	//criando o statement para fazer a alteração no banco
+	statement, error := db.Prepare("update Recipes set name = ?, description = ?, image = ?, categoryId = ?, prepare_mode = ? where id = ?")
+	if error != nil {
+		rw.Write([]byte("Error when create statement!"))
+		return
+	}
+	defer statement.Close()
+
+	//executando o comando sql do statement criado anteriormente
+	if _, error := statement.Exec(recipe.Name, recipe.Description, recipe.Image, recipe.CategoryId, recipe.Prepare_mode, ID); error != nil {
+		rw.Write([]byte("Error when update recipe"))
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
 
 }
 
 func DeleteRecipe(rw http.ResponseWriter, r *http.Request) {
+	//recuperando o parametro passado na requisição
+	parameters := mux.Vars(r)
+	ID, error := strconv.ParseInt(parameters["id"], 10, 32)
+	if error != nil {
+		rw.Write([]byte("Error to convter parameter to integer"))
+		return
+	}
+
+	//conectando com o banco
+	db, error := database.Connect()
+	if error != nil {
+		rw.Write([]byte("Error to connect to database"))
+		return
+	}
+
+	defer db.Close()
+
+	//criando o statement para executar no banco
+	statement, error := db.Prepare("delete from Recipes where id = ?")
+	if error != nil {
+		rw.Write([]byte("Error when create statement"))
+	}
+	defer statement.Close()
+
+	//executando o statement
+	if _, error := statement.Exec(ID); error != nil {
+		rw.Write([]byte("Error to delete recipe"))
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
+
+}
+
+func CreateIngredient(rw http.ResponseWriter, r *http.Request) {
+
+	//lendo o corpo da requisição
+	requestBody, error := ioutil.ReadAll(r.Body)
+	if error != nil {
+		rw.Write([]byte("Request body read eror"))
+	}
+
+	//convertendo o json do boyd da requisição para struct
+	var ingredient ingredient
+	if error := json.Unmarshal(requestBody, &ingredient); error != nil {
+		rw.Write([]byte("Error to convert ingredient to struct"))
+		return
+	}
+
+	//abrindo conexão com obanco
+	db, error := database.Connect()
+	if error != nil {
+		rw.Write([]byte("Error to connect to database"))
+		return
+	}
+	defer db.Close()
+
+	//criando o statement para manipular o banco de dados
+	statement, error := db.Prepare("insert into Ingredients (name, recipeId) values (?,?)")
+	if error != nil {
+		rw.Write([]byte("Error to create statement"))
+		return
+	}
+
+	defer statement.Close()
+
+	//Executando o statement
+	insert, error := statement.Exec(ingredient.Name, ingredient.RecipeId)
+	if error != nil {
+		rw.Write([]byte("Error when running statement"))
+		return
+	}
+
+	//recuperando o id do novo registro no banco
+	insertedId, error := insert.LastInsertId()
+	if error != nil {
+		rw.Write([]byte("Erro when retrieve inserted id"))
+		return
+	}
+
+	//status codes
+	rw.WriteHeader(http.StatusCreated)
+	rw.Write([]byte(fmt.Sprintf("Ingredient inserted with id: %d", insertedId)))
+
+}
+
+func ShowIngredients(rw http.ResponseWriter, r *http.Request) {
+
+	//Pegando os paramentros da requisição
+	parameters := mux.Vars(r)
+	ID, error := strconv.ParseInt(parameters["id"], 10, 32)
+
+	if error != nil {
+		rw.Write([]byte("Error while convert parameter to integer"))
+		return
+	}
+
+	//Conectando com o banco
+	db, error := database.Connect()
+	if error != nil {
+		rw.Write([]byte("Error to connect to database"))
+		return
+	}
+
+	defer db.Close()
+
+	//Buscar todos os registros no banco
+	lines, error := db.Query("select * from Ingredients where recipeId=?", ID)
+	if error != nil {
+		rw.Write([]byte("Error when search ingredients"))
+		return
+	}
+	defer lines.Close()
+
+	var ingredients []ingredient
+	for lines.Next() {
+		var ingredient ingredient
+		if error := lines.Scan(&ingredient.ID, &ingredient.Name, &ingredient.RecipeId); error != nil {
+			rw.Write([]byte("Error when search ingredients"))
+			return
+		}
+
+		ingredients = append(ingredients, ingredient)
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	if error := json.NewEncoder(rw).Encode(ingredients); error != nil {
+		rw.Write([]byte("Error when convert ingredients to JSON"))
+		return
+	}
+
+}
+
+func UpdateIngredient(rw http.ResponseWriter, r *http.Request) {
+	parameters := mux.Vars(r)
+	ID, error := strconv.ParseInt(parameters["id"], 10, 32)
+	if error != nil {
+		rw.Write([]byte("Error when convert parameter to integer"))
+		return
+	}
+
+	//pegando o corpo da requisição
+	requestBody, error := ioutil.ReadAll(r.Body)
+	if error != nil {
+		rw.Write([]byte("Error when read request body"))
+		return
+	}
+
+	var ingredient ingredient
+	if error := json.Unmarshal(requestBody, &ingredient); error != nil {
+		rw.Write([]byte("Error when convert ingredients to struct"))
+		return
+	}
+
+	//conectando com o banco
+	db, error := database.Connect()
+	if error != nil {
+		rw.Write([]byte("Error to connect to database"))
+		return
+	}
+	defer db.Close()
+
+	//criando o statement para fazer a alteração no banco
+	statement, error := db.Prepare("update Ingredients set name=?, recipeId = ? where id = ?")
+	if error != nil {
+		rw.Write([]byte("Error when create statement"))
+		return
+	}
+	defer statement.Close()
+
+	//executando o comando sql criado anteriormente
+	if _, error := statement.Exec(ingredient.Name, ingredient.RecipeId, ID); error != nil {
+		fmt.Println(error)
+		rw.Write([]byte("Error when update ingredient"))
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
+
+}
+
+func DeleteIngredient(rw http.ResponseWriter, r *http.Request) {
+
+	//recuperando o parametro passado na requisição
+	parameters := mux.Vars(r)
+	ID, error := strconv.ParseInt(parameters["id"], 10, 32)
+	if error != nil {
+		rw.Write([]byte("Error to convert parameter to integer"))
+		return
+	}
+
+	//conectando com o banco
+	db, error := database.Connect()
+	if error != nil {
+		rw.Write([]byte("Error to convert parameter to integer"))
+		return
+	}
+	defer db.Close()
+
+	//criando o statement para executar no banco
+	statement, error := db.Prepare("delete from Ingredients where id = ?")
+	if error != nil {
+		rw.Write([]byte("Error when create statement"))
+		return
+	}
+	defer statement.Close()
+
+	//executando o statement
+	if _, error := statement.Exec(ID); error != nil {
+		rw.Write([]byte("Error to delete ingredient"))
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
 
 }
